e6693e3b9aba4e0c589dd60a17a5f836ee042f72
elliottwu <swuai@connect.ust.hk>
2018-07-28 02:03:00 +0800

    minor fix

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,4 +1,3 @@
 wget "https://storage.googleapis.com/deephdr/kalantari_dataset.zip"
 unzip kalantari_dataset.zip
-mv train test dataset/
 rm kalantari_dataset.zip
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --dataset ./dataset/tf_records --batch_size 16 --checkpoint_dir checkpoints_1 --sample_dir samples_1 --log_dir logs_1 --epoch 50 --learning_rate 0.0002 --load_size 256
+python train.py --dataset ./dataset/tf_records --batch_size 16 --checkpoint_dir checkpoints --sample_dir samples --log_dir logs --epoch 50 --learning_rate 0.0002 --load_size 256
