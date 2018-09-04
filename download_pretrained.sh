cf67d0a42ffee9be3eb9d4fa6aa361e2499467d1
elliottwu <swuai@connect.ust.hk>
2018-09-04 12:16:09 +0800

    updated links

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -1,3 +1,3 @@
-wget "https://storage.googleapis.com/deephdr/pretrained_ckp.zip"
+wget -O pretrained_ckp.zip "https://unioxfordnexus-my.sharepoint.com/:u:/g/personal/sann6319_ox_ac_uk/ES75DdC2CrRLvbP0DYWN368Bsn2QlljzHJKYOdIRWREFhA?download=1"
 unzip pretrained_ckp.zip
 rm pretrained_ckp.zip
