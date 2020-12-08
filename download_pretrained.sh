be8ac659dc77d0373847865735204e7262f01592
elliottwu <elliottwu@users.noreply.github.com>
2020-12-08 16:10:18 +0800

    updated download links

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -1,3 +1,3 @@
-wget -O pretrained_ckp.zip "https://unioxfordnexus-my.sharepoint.com/:u:/g/personal/sann6319_ox_ac_uk/ES75DdC2CrRLvbP0DYWN368Bsn2QlljzHJKYOdIRWREFhA?download=1"
+wget -O pretrained_ckp.zip "https://www.robots.ox.ac.uk/~szwu/storage/hdr/pretrained_ckp.zip"
 unzip pretrained_ckp.zip
 rm pretrained_ckp.zip
