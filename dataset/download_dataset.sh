fea249e892b8429eaff685051bda4b87ba994959
elliottwu <elliottwu@users.noreply.github.com>
2020-12-08 16:11:03 +0800

    updated download links

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,3 +1,3 @@
-wget -O kalantari_dataset.zip "https://unioxfordnexus-my.sharepoint.com/:u:/g/personal/sann6319_ox_ac_uk/EXAEKKzmZrRPiHjgrh35g8QBmn5YF5hrd08z6rAH7YpORA?download=1"
+wget -O kalantari_dataset.zip "https://www.robots.ox.ac.uk/~szwu/storage/hdr/kalantari_dataset.zip"
 unzip kalantari_dataset.zip
 rm kalantari_dataset.zip
