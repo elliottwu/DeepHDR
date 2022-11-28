16a1246626374b8139ef89c8c290a55315c88f8f
elliottwu <elliottwu@users.noreply.github.com>
2022-11-28 16:51:05 +0000

    updated links

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,3 +1,3 @@
-wget -O kalantari_dataset.zip "https://www.robots.ox.ac.uk/~szwu/storage/hdr/kalantari_dataset.zip"
+wget -O kalantari_dataset.zip "https://www.robots.ox.ac.uk/~szwu/storage/18_hdr/kalantari_dataset.zip"
 unzip kalantari_dataset.zip
 rm kalantari_dataset.zip
--- download_pretrained.sh
+++ download_pretrained.sh
@@ -1,3 +1,3 @@
-wget -O pretrained_ckp.zip "https://www.robots.ox.ac.uk/~szwu/storage/hdr/pretrained_ckp.zip"
+wget -O pretrained_ckp.zip "https://www.robots.ox.ac.uk/~szwu/storage/18_hdr/pretrained_ckp.zip"
 unzip pretrained_ckp.zip
 rm pretrained_ckp.zip
